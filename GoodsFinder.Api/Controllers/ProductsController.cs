1bcc854edd3542be003932deafdbc19ea76ace05
LukaszIdz <lukashid@gmail.com>
2019-03-26 16:41:12 +0100

    added sln - project structure settings

--- GoodsFinder.Api/Controllers/ProductsController.cs
+++ GoodsFinder.Api/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GoodsFinder.Controllers
+namespace GoodsFinder.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- GoodsFinder.Api/Startup.cs
+++ GoodsFinder.Api/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,11 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace GoodsFinder
+namespace GoodsFinder.Api
 {
     public class Startup
     {
@@ -26,12 +25,11 @@ namespace GoodsFinder
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddNewtonsoftJson();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -44,13 +42,7 @@ namespace GoodsFinder
             }
 
             app.UseHttpsRedirection();
-
-            app.UseRouting(routes =>
-            {
-                routes.MapControllers();
-            });
-
-            app.UseAuthorization();
+            app.UseMvc();
         }
     }
 }
